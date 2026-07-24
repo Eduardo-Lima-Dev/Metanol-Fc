318c034c8b3c7d0875a411330ba1f777253be53a
Kauã Lima <dev.kaualima@gmail.com>
2026-07-24 03:50:34 -0300

    feat: atualizando a main

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ZodValidationPipe } from 'nestjs-zod';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+  app.useGlobalPipes(new ZodValidationPipe());
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT ?? 3338);
+  console.log(`🚀 API running on http://localhost:${process.env.PORT}/api`);
 }
+
 bootstrap();

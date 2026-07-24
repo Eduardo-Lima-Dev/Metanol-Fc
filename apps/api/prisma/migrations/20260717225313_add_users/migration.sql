b6cf828a0dbdb79d02d72af176fed89c7c221ab1
Kauã Lima <dev.kaualima@gmail.com>
2026-07-24 03:47:03 -0300

    db: add migrações

---
+++ apps/api/prisma/migrations/20260717225313_add_users/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "nickname" TEXT,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_nickname_key" ON "Users"("nickname");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
